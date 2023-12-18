19599e22c65756d150a37a91af1fcd69a83de680
gaogao-qwq <gaogaoqwq@gmail.com>
2023-12-18 17:00:44 +0800

    (Backend)Chore: Development only, enable CORS.
    Signed-off-by: gaogao-qwq <gaogaoqwq@gmail.com>

---
+++ backend/src/main/java/com/gaogaoqwq/mall/config/WebMvcConfig.java
@@ -0,0 +1,19 @@
+package com.gaogaoqwq.mall.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebMvcConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost")
+                .allowedOriginPatterns("*")
+                .allowedHeaders("*")
+                .exposedHeaders("*")
+                .allowCredentials(true)
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH");
+    }
+}
