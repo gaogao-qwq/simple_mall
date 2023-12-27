cc3d31595af20a9d1cbd8a69168a9112f12778c3
gaogao-qwq <gaogaoqwq@gmail.com>
2023-12-27 23:17:19 +0800

    Feat: Add view to show count of entity
    Signed-off-by: gaogao-qwq <gaogaoqwq@gmail.com>

---
+++ backend/src/main/java/com/gaogaoqwq/mall/view/CountView.java
@@ -0,0 +1,14 @@
+package com.gaogaoqwq.mall.view;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder
+@AllArgsConstructor
+public class CountView {
+
+    private long count;
+
+}
