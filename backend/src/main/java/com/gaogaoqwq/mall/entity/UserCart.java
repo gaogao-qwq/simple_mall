a8462825ddb460160ca4f4fc718bac2c9983c744
gaogao-qwq <gaogaoqwq@gmail.com>
2023-12-27 23:31:07 +0800

    (Backend)Feat: Add UserCart entity
    Signed-off-by: gaogao-qwq <gaogaoqwq@gmail.com>

---
+++ backend/src/main/java/com/gaogaoqwq/mall/entity/UserCart.java
@@ -0,0 +1,31 @@
+package com.gaogaoqwq.mall.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Collection;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity
+@Table(name = "user_cart")
+public class UserCart {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private String id;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    @ManyToMany(cascade = CascadeType.ALL)
+    @JoinTable(name = "user_cart_good")
+    private Collection<Good> goods;
+
+}
