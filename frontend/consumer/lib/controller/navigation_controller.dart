1aa54d7279c723b6d6e1650b8238906f24b24ab6
gaogao-qwq <gaogaoqwq@gmail.com>
2023-12-27 15:22:06 +0800

    (Frontend)Feat: Add NavigationBarController to manage navigation state

---
+++ frontend/consumer/lib/controller/navigation_controller.dart
@@ -0,0 +1,22 @@
+import 'package:consumer/pages/home_page.dart';
+import 'package:consumer/pages/my_page.dart';
+import 'package:consumer/pages/shopping_cart_page.dart';
+import 'package:get/get.dart';
+
+const navigations = [HomePage(), ShoppingCartPage(), MyPage()];
+
+class NavigationBarController extends GetxController {
+  var currentIndex = 0.obs;
+
+  void changeIndex(int index) {
+    currentIndex.value = index;
+    Get.off(
+      navigations[index],
+      transition: Transition.noTransition,
+    );
+  }
+
+  bool isDestinationSelected(int index) {
+    return currentIndex.value == index;
+  }
+}
