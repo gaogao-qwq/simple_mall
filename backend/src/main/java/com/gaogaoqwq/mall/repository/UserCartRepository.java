6503fbaa4bec0d8ada37a0103515e9b5c163faa9
gaogao-qwq <gaogaoqwq@gmail.com>
2024-01-02 22:58:18 +0800

    (Backend)Feat: Add findUserCartByUserId method in UserCartRepository
    Signed-off-by: gaogao-qwq <gaogaoqwq@gmail.com>

--- backend/src/main/java/com/gaogaoqwq/mall/repository/UserCartRepository.java
+++ backend/src/main/java/com/gaogaoqwq/mall/repository/UserCartRepository.java
@@ -1,7 +1,13 @@
 package com.gaogaoqwq.mall.repository;
 
 import com.gaogaoqwq.mall.entity.UserCart;
+
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserCartRepository extends JpaRepository<UserCart, String> {
+
+    public Optional<UserCart> findUserCartByUserId(Long userId);
+
 }
