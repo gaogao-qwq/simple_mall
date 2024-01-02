e8effc3d147b586e701d78b0905b434a64c45d24
gaogao-qwq <gaogaoqwq@gmail.com>
2024-01-02 22:58:30 +0800

    (Backend)Chore: Remove unnecessary imports
    Signed-off-by: gaogao-qwq <gaogaoqwq@gmail.com>

--- backend/src/main/java/com/gaogaoqwq/mall/repository/GoodRepository.java
+++ backend/src/main/java/com/gaogaoqwq/mall/repository/GoodRepository.java
@@ -1,7 +1,6 @@
 package com.gaogaoqwq.mall.repository;
 
 import com.gaogaoqwq.mall.entity.Good;
-import org.hibernate.query.Page;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface GoodRepository extends JpaRepository<Good, Long> {}
