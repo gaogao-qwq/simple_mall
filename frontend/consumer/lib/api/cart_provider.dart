09f317a1ac7ed59c273233ad74e3b9efcf391e54
gaogao-qwq <gaogaoqwq@gmail.com>
2024-01-03 22:22:12 +0800

    (Frontend)Feat: Add cart-related method to cart_provider
    Signed-off-by: gaogao-qwq <gaogaoqwq@gmail.com>

--- frontend/consumer/lib/api/cart_provider.dart
+++ frontend/consumer/lib/api/cart_provider.dart
@@ -1,6 +1,37 @@
+import 'package:consumer/api/api_response.dart';
 import 'package:consumer/controller/user_detail_controller.dart';
+import 'package:consumer/type/cart_item.dart';
 import 'package:get/get.dart';
 
 class CartProvider extends GetConnect {
+  final udc = Get.put(UserDetailController());
   final uc = Get.put(UserDetailController());
+
+  @override
+  void onInit() {
+    httpClient.baseUrl = 'http://localhost:8080';
+    httpClient.addRequestModifier<dynamic>((request) {
+      if (udc.accessToken.isEmpty) return request;
+      request.headers['Authorization'] = 'Bearer ${udc.accessToken}';
+      return request;
+    });
+  }
+
+  Future<List<CartItem>> getCartItems() async {
+    var response = (await get(
+      "/v1/customer/cart",
+      decoder: (data) => ApiResponse.fromJson(data)
+    )).body;
+    if (response == null || response.data == null) {
+      return [];
+    }
+    return (response.data as List).map((e) => CartItem.fromJson(e)).toList();
+  }
+
+  Future<void> removeItemFromCart(int goodId) async {
+    await delete(
+      "/v1/customer/cart?good_id=$goodId",
+      decoder: (data) => ApiResponse.fromJson(data)
+    );
+  }
 }
