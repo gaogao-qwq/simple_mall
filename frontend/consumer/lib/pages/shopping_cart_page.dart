a67c2dcf841a7737c3faa2c0d7f0753ece9e9755
gaogao-qwq <gaogaoqwq@gmail.com>
2024-01-04 16:42:28 +0800

    (Frontend)Feat: Initialize shopping_cart_page layout
    Signed-off-by: gaogao-qwq <gaogaoqwq@gmail.com>

--- frontend/consumer/lib/pages/shopping_cart_page.dart
+++ frontend/consumer/lib/pages/shopping_cart_page.dart
@@ -1,9 +1,60 @@
 import 'package:consumer/api/cart_provider.dart';
 import 'package:consumer/components/mall_navigation_bar.dart';
+import 'package:consumer/controller/shopping_cart_controller.dart';
 import 'package:consumer/controller/user_detail_controller.dart';
 import 'package:consumer/pages/auth_page.dart';
+import 'package:extended_image/extended_image.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+
+class CartListItem extends StatelessWidget {
+  final ExtendedImage thumbnail;
+  final String goodName;
+  final String goodPrice;
+
+  const CartListItem({
+    super.key,
+    required this.thumbnail,
+    required this.goodName,
+    required this.goodPrice,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: 100,
+      child: Card(
+        child: InkWell(
+          onTap: () {},
+          child: Row(
+            children: [
+              ClipRRect(
+                borderRadius: BorderRadius.circular(12),
+                child: thumbnail,
+              ),
+              const SizedBox(width: 8),
+              Column(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(goodName),
+                  Row(
+                    textBaseline: TextBaseline.alphabetic,
+                    crossAxisAlignment: CrossAxisAlignment.baseline,
+                    children: [
+                      const Text("¥"),
+                      Text(goodPrice, style: const TextStyle(fontSize: 18)),
+                    ]
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
 
 class ShoppingCartPage extends StatelessWidget {
   const ShoppingCartPage({super.key});
@@ -35,9 +86,48 @@ class ShoppingCartPage extends StatelessWidget {
       ),
     );
 
+    Widget cartListView = FutureBuilder(
+      future: cp.getCartItems(),
+      builder: (ctx, snp) {
+        if (snp.hasError) {
+          return const Center(child: Text("获取购物车信息时出错"));
+        }
+
+        if (snp.hasData) {
+          if (snp.data!.isEmpty) {
+            return const Center(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Icon(Icons.shopping_cart_outlined),
+                  Text("购物车里空空如也"),
+                ],
+              ),
+            );
+          }
+          return ListView.builder(
+            itemCount: snp.data!.length,
+            itemBuilder: (ctx, idx) => CartListItem(
+              thumbnail: ExtendedImage.network(
+                snp.data![idx].previewImgUrl,
+                width: 100,
+                height: 100,
+                fit: BoxFit.cover,
+                clearMemoryCacheIfFailed: true,
+              ),
+              goodName: snp.data![idx].goodName,
+              goodPrice: snp.data![idx].price
+            ),
+          );
+        }
+
+        return const Center(child: CircularProgressIndicator(semanticsLabel: "加载购物车信息中"));
+      },
+    );
+
     return Scaffold(
       appBar: AppBar(title: const Text("购物车")),
-      body: udc.isLogin() ? ListView() : guestPlaceholder,
+      body: udc.isLogin() ? cartListView : guestPlaceholder,
       bottomNavigationBar: const MallNavigationBar(),
     );
   }
