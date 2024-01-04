f98504566bf945ac723c5aad1ae8aaf854fa154f
gaogao-qwq <gaogaoqwq@gmail.com>
2024-01-04 16:41:39 +0800

    (Backend)Chore: Apply refactor
    Signed-off-by: gaogao-qwq <gaogaoqwq@gmail.com>

--- backend/src/main/java/com/gaogaoqwq/mall/service/impl/CustomerServiceImpl.java
+++ backend/src/main/java/com/gaogaoqwq/mall/service/impl/CustomerServiceImpl.java
@@ -3,14 +3,16 @@ package com.gaogaoqwq.mall.service.impl;
 import com.gaogaoqwq.mall.view.CustomerCartItemView;
 import com.gaogaoqwq.mall.entity.Good;
 import com.gaogaoqwq.mall.entity.User;
-import com.gaogaoqwq.mall.entity.UserCart;
-import com.gaogaoqwq.mall.repository.UserCartRepository;
+import com.gaogaoqwq.mall.entity.Cart;
+import com.gaogaoqwq.mall.entity.CartItem;
+import com.gaogaoqwq.mall.repository.CartRepository;
 import com.gaogaoqwq.mall.repository.UserRepository;
 import com.gaogaoqwq.mall.service.CustomerService;
 import lombok.RequiredArgsConstructor;
 
 import org.springframework.stereotype.Service;
 
+import java.time.Instant;
 import java.util.List;
 import java.util.Optional;
 
@@ -19,34 +21,40 @@ import java.util.Optional;
 public class CustomerServiceImpl implements CustomerService {
 
     private final UserRepository userRepo;
-    private final UserCartRepository userCartRepo;
+    private final CartRepository cartRepo;
 
     @Override
     public List<CustomerCartItemView> getCartItemsByUsername(String username) {
         Optional<User> userOpt = userRepo.findUserByUsername(username);
         if (userOpt.isEmpty() || !userOpt.get().isEnabled()) return List.of();
         final User user = userOpt.get();
-        final Optional<UserCart> userCartOpt = userCartRepo.findUserCartByUserId(user.getId());
+        final Optional<Cart> userCartOpt = cartRepo.findUserCartByUserId(user.getId());
         if (userCartOpt.isEmpty()) return List.of();
-        return userCartOpt.get().getGoods().stream().map(CustomerCartItemView::fromGood).toList();
+        return userCartOpt.get().getCartItems().stream().map(e -> {
+            return CustomerCartItemView.fromGood(e.getGood());
+        }).toList();
     }
 
     @Override
     public void addGoodToCart(Good good, User user) {
-        Optional<UserCart> userCartOpt = userCartRepo.findUserCartByUserId(user.getId());
-        if (userCartOpt.isEmpty()) return;
-        final UserCart userCart = userCartOpt.get();
-        userCart.getGoods().add(good);
-        userCartRepo.save(userCart);
+        Optional<Cart> cartOpt = cartRepo.findUserCartByUserId(user.getId());
+        if (cartOpt.isEmpty()) return;
+        final var cart = cartOpt.get();
+        cart.getCartItems().add(CartItem.builder()
+            .good(good)
+            .cart(cart)
+            .addDate(Instant.now())
+            .build());
+        cartRepo.save(cart);
     }
 
     @Override
     public void removeGoodFromCart(Long goodId, User user) {
-        Optional<UserCart> userCartOpt = userCartRepo.findUserCartByUserId(user.getId());
-        if (userCartOpt.isEmpty()) return;
-        final UserCart userCart = userCartOpt.get();
-        userCart.getGoods().removeIf(g -> g.getId().equals(goodId));
-        userCartRepo.save(userCart);
+        Optional<Cart> cartOpt = cartRepo.findUserCartByUserId(user.getId());
+        if (cartOpt.isEmpty()) return;
+        final Cart cart = cartOpt.get();
+        cart.getCartItems().removeIf(e -> e.getGood().getId().equals(goodId));
+        cartRepo.save(cart);
     }
 
 }
