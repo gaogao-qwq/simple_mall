bc31f1879e3e3077f7743765c99aacccf5a6fbcc
gaogao-qwq <gaogaoqwq@gmail.com>
2024-01-09 13:40:18 +0800

    (Backend)Feat: Implement setCartItemCountByID method in CustomerService
    Signed-off-by: gaogao-qwq <gaogaoqwq@gmail.com>

--- backend/src/main/java/com/gaogaoqwq/mall/service/CustomerService.java
+++ backend/src/main/java/com/gaogaoqwq/mall/service/CustomerService.java
@@ -10,6 +10,8 @@ public interface CustomerService {
 
     public List<CartItemView> getCartItemsByUsername(String username);
 
+    public void setCartItemCountById(String id, Integer count, User user);
+
     public void addGoodToCart(Good good, User user);
     
     public void removeGoodFromCart(Long goodId, User user);
--- backend/src/main/java/com/gaogaoqwq/mall/service/impl/CustomerServiceImpl.java
+++ backend/src/main/java/com/gaogaoqwq/mall/service/impl/CustomerServiceImpl.java
@@ -5,6 +5,7 @@ import com.gaogaoqwq.mall.entity.Good;
 import com.gaogaoqwq.mall.entity.User;
 import com.gaogaoqwq.mall.entity.Cart;
 import com.gaogaoqwq.mall.entity.CartItem;
+import com.gaogaoqwq.mall.repository.CartItemRepository;
 import com.gaogaoqwq.mall.repository.CartRepository;
 import com.gaogaoqwq.mall.repository.UserRepository;
 import com.gaogaoqwq.mall.service.CustomerService;
@@ -22,6 +23,7 @@ public class CustomerServiceImpl implements CustomerService {
 
     private final UserRepository userRepo;
     private final CartRepository cartRepo;
+    private final CartItemRepository cartItemRepo;
 
     @Override
     public List<CartItemView> getCartItemsByUsername(String username) {
@@ -59,6 +61,16 @@ public class CustomerServiceImpl implements CustomerService {
         final Cart cart = cartOpt.get();
         cart.getCartItems().removeIf(e -> e.getGood().getId().equals(goodId));
         cartRepo.save(cart);
+    }
+
+    @Override
+    public void setCartItemCountById(String id, Integer count, User user) {
+        Optional<CartItem> cartItemOpt =cartItemRepo.findById(id);
+        cartItemOpt.ifPresent(e -> {
+            if (!cartItemOpt.get().getCart().getUser().equals(user)) return;
+            cartItemOpt.get().setCount(count);
+            cartItemRepo.save(cartItemOpt.get());
+        });
     }
 
 }
