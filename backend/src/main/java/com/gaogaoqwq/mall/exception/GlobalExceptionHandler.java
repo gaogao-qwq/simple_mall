d2774c4a79186e00ad5ef6e7a283a003db463c51
gaogao-qwq <gaogaoqwq@gmail.com>
2024-01-09 13:42:18 +0800

    (Backend)Feat: Handle NoResourceFoundException explicitly
    Signed-off-by: gaogao-qwq <gaogaoqwq@gmail.com>

--- backend/src/main/java/com/gaogaoqwq/mall/exception/GlobalExceptionHandler.java
+++ backend/src/main/java/com/gaogaoqwq/mall/exception/GlobalExceptionHandler.java
@@ -5,12 +5,23 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.servlet.resource.NoResourceFoundException;
 
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
 @ControllerAdvice
 public class GlobalExceptionHandler {
 
+    @ExceptionHandler(NoResourceFoundException.class)
+    public ResponseEntity<R> handleNoResourceFoundException(NoResourceFoundException e) {
+        log.info("Handled exception: {}", e.getClass().getName());
+        return new ResponseEntity<>(R.failure(e.getMessage()), HttpStatus.NOT_FOUND);
+    }
+
     @ExceptionHandler(Exception.class)
     public ResponseEntity<R> handleException(Exception e) {
+        log.warn("Unhandled exception response: {}", e.getClass().getName());
         return new ResponseEntity<>(R.failure(e.getMessage()), HttpStatus.INTERNAL_SERVER_ERROR);
     }
 
