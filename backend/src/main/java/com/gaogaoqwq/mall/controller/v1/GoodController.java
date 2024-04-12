46563ab7f9c84220bfd69d8a0cf799237df8a222
gaogao-qwq <gaogaoqwq@gmail.com>
2024-04-12 14:48:19 +0800

    (B)Chore: Implement MinIO logic in GoodController
    Signed-off-by: gaogao-qwq <gaogaoqwq@gmail.com>

--- backend/src/main/java/com/gaogaoqwq/mall/controller/v1/GoodController.java
+++ backend/src/main/java/com/gaogaoqwq/mall/controller/v1/GoodController.java
@@ -1,15 +1,20 @@
 package com.gaogaoqwq.mall.controller.v1;
 
+import com.gaogaoqwq.mall.entity.Good;
+import com.gaogaoqwq.mall.entity.GoodSwiper;
 import com.gaogaoqwq.mall.enums.ErrorMessage;
 import com.gaogaoqwq.mall.response.R;
 import com.gaogaoqwq.mall.service.GoodService;
+import com.gaogaoqwq.mall.service.impl.MinioServiceImpl;
 import com.gaogaoqwq.mall.view.CountView;
 import com.gaogaoqwq.mall.view.GoodDetailView;
 import com.gaogaoqwq.mall.view.GoodInfoView;
 import com.gaogaoqwq.mall.view.GoodSwiperView;
 import lombok.RequiredArgsConstructor;
+
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
 import java.util.Optional;
 
 @RestController
@@ -18,43 +23,91 @@ import java.util.Optional;
 public class GoodController {
 
     final private GoodService goodService;
+    final private MinioServiceImpl minioService;
 
     @GetMapping("/list")
-    public R goodList(@RequestParam int page, @RequestParam(required = false) Optional<Integer> size) {
-        if (size.isEmpty()) size = Optional.of(10);
-        var goods = goodService.getGoodByPage(page, size.get());
-        var views = goods.stream().map(GoodInfoView::fromGood).toList();
+    public R goodList(@RequestParam int page,
+            @RequestParam(required = false) Optional<Integer> size) {
+        if (size.isEmpty())
+            size = Optional.of(10);
+        List<Good> goods = goodService.getGoodByPage(page, size.get());
+
+        List<GoodInfoView> views = goods.stream()
+                .map(GoodInfoView::fromGood)
+                .toList();
+        views = views.stream().map(e -> {
+            try {
+                e.setImgUrl(minioService.getObjectUrl(e.getImgUrl()));
+            } catch (Exception ex) {
+                ex.printStackTrace();
+                e.setImgUrl(String.valueOf(""));
+            }
+            return e;
+        }).toList();
+
         return R.successBuilder()
-            .data(views)
-            .build();
+                .data(views)
+                .build();
     }
 
     @GetMapping("/count")
     public R goodCount() {
         return R.successBuilder()
-            .data(new CountView(goodService.getGoodCount()))
-            .build();
+                .data(new CountView(goodService.getGoodCount()))
+                .build();
     }
 
     @GetMapping("/swiper")
     public R goodSwiper() {
-        var goodSwiper = goodService.getGoodSwiper();
-        var views = goodSwiper.stream().map(GoodSwiperView::fromGoodSwiper).toList();
+        List<GoodSwiper> goodSwiper = goodService.getGoodSwiper();
+        List<GoodSwiperView> views = goodSwiper.stream()
+                .map(GoodSwiperView::fromGoodSwiper)
+                .toList();
+
+        views = views.stream().map(e -> {
+            try {
+                e.setImgUrl(minioService.getObjectUrl(e.getImgUrl()));
+            } catch (Exception ex) {
+                ex.printStackTrace();
+                e.setImgUrl(String.valueOf(""));
+            }
+            return e;
+        }).toList();
+
         return R.successBuilder()
-            .data(views)
-            .build();
+                .data(views)
+                .build();
     }
 
     @GetMapping("/detail/{id}")
     public R goodDetail(@PathVariable Long id) {
-        var good = goodService.getGoodById(id);
-        return good.map(value -> R.successBuilder()
-                    .data(GoodDetailView.fromGood(value))
-                    .build())
-                .orElseGet(() -> R.successBuilder()
-                    .success(false)
+        Optional<Good> good = goodService.getGoodById(id);
+        if (good.isEmpty()) {
+            return R.failureBuilder()
                     .message(ErrorMessage.GOOD_NOT_EXIST)
-                    .build());
+                    .build();
+        }
+        GoodDetailView view = GoodDetailView.fromGood(good.get());
+
+        try {
+            view.setPreviewImgUrl(minioService.getObjectUrl(view.getPreviewImgUrl()));
+        } catch (Exception e) {
+            view.setPreviewImgUrl(String.valueOf(""));
+        }
+
+        view.setDetailImgUrl(
+                view.getDetailImgUrl().stream().map(e -> {
+                    try {
+                        return minioService.getObjectUrl(e);
+                    } catch (Exception ex) {
+                        ex.printStackTrace();
+                        return String.valueOf("");
+                    }
+                }).toList());
+
+        return R.successBuilder()
+                .data(view)
+                .build();
     }
 
 }
