9456b4d54a9f97866c8417b075ecf40f6e3ed990
gaogao-qwq <gaogaoqwq@gmail.com>
2024-04-13 22:15:20 +0800

    (B)Feat: Add more query method to UserRepository
    Signed-off-by: gaogao-qwq <gaogaoqwq@gmail.com>

--- backend/src/main/java/com/gaogaoqwq/mall/repository/UserRepository.java
+++ backend/src/main/java/com/gaogaoqwq/mall/repository/UserRepository.java
@@ -2,13 +2,34 @@ package com.gaogaoqwq.mall.repository;
 
 import com.gaogaoqwq.mall.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.transaction.annotation.Transactional;
 
+import java.util.Date;
+import java.util.List;
 import java.util.Optional;
 
+@Transactional(readOnly = true)
 public interface UserRepository extends JpaRepository<User, Long> {
 
     boolean existsByUsername(String username);
 
     Optional<User> findUserByUsername(String username);
+
+    List<User> findByCreateDateAfter(Date date);
+
+    List<User> findByCreateDateBefore(Date date);
+
+    List<User> findByCreateDateBetween(Date startDate, Date endDate);
+
+    @Modifying
+    @Transactional
+    @Query("update User u set u.enable = ?1 where u.id = ?2")
+    void toggleUserEnableById(Boolean enable, Long id);
+
+    @Modifying
+    @Query("delete from User u where u.enable = false")
+    void deleteDisabledUser();
 
 }
