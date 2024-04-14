8308634161f2aec9b45e085c17f561ffb72526a2
gaogao-qwq <gaogaoqwq@gmail.com>
2024-04-14 18:19:30 +0800

    (FC)Chore: Set zh_CN as default l10n
    Signed-off-by: gaogao-qwq <gaogaoqwq@gmail.com>

--- frontend/consumer/lib/main.dart
+++ frontend/consumer/lib/main.dart
@@ -3,6 +3,7 @@ import 'package:consumer/controller/app_setting_controller.dart';
 import 'package:consumer/controller/user_detail_controller.dart';
 import 'package:consumer/pages/home_page.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:get/get.dart';
 import 'package:get_storage/get_storage.dart';
 
@@ -29,20 +30,26 @@ class MyApp extends StatelessWidget {
     final asc = Get.put(AppSettingController());
 
     return GetMaterialApp(
-      title: '商城',
+      title: "简商",
+      home: const HomePage(),
+      locale: const Locale("zh", "CN"),
+      localizationsDelegates: const [
+        GlobalMaterialLocalizations.delegate,
+        GlobalWidgetsLocalizations.delegate,
+        GlobalCupertinoLocalizations.delegate,
+      ],
+      supportedLocales: const [Locale("en", "US"), Locale("zh", "CN")],
       theme: ThemeData(
         colorSchemeSeed: Colors.deepPurple,
         useMaterial3: true,
         brightness: Brightness.light,
       ),
       darkTheme: ThemeData(
-        colorSchemeSeed: Colors.deepPurple,
-        useMaterial3: true,
-        brightness: Brightness.dark
-      ),
+          colorSchemeSeed: Colors.deepPurple,
+          useMaterial3: true,
+          brightness: Brightness.dark),
       themeMode: asc.themeMode.value,
       debugShowCheckedModeBanner: false,
-      home: const HomePage(),
     );
   }
 }
