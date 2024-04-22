f8fffda52079dc8febfb400072c203966d772446
gaogao-qwq <gaogaoqwq@gmail.com>
2024-04-22 10:42:45 +0800

    (FC)Feat: Add entry point of AddressBookPage in MyPage
    Signed-off-by: gaogao-qwq <gaogaoqwq@gmail.com>

--- frontend/consumer/lib/pages/my_page.dart
+++ frontend/consumer/lib/pages/my_page.dart
@@ -3,6 +3,7 @@ import 'package:consumer/components/mall_navigation_rail.dart';
 import 'package:consumer/controller/shopping_cart_controller.dart';
 import 'package:consumer/controller/user_detail_controller.dart';
 import 'package:consumer/enum/layout_size.dart';
+import 'package:consumer/pages/address_book_page.dart';
 import 'package:consumer/pages/auth_page.dart';
 import 'package:consumer/pages/settings_page.dart';
 import 'package:flutter/material.dart';
@@ -16,55 +17,81 @@ class MyPage extends StatelessWidget {
     final udc = Get.put(UserDetailController());
     final scc = Get.put(ShoppingCartController());
 
-    Widget myPageListView = Column(
-      children: [
-        Card(
-            child: Padding(
-                padding:
-                    const EdgeInsets.symmetric(vertical: 16, horizontal: 8),
-                child: ListTile(
-                    leading: CircleAvatar(
-                      radius: 30,
-                      backgroundImage: Image.asset("images/avatar.png").image,
-                    ),
-                    title: Obx(() => udc.isLogin()
-                        ? Text(udc.username.value,
-                            style: const TextStyle(fontSize: 24))
-                        : const Text("未登录，请先登录")),
-                    trailing: const Icon(Icons.keyboard_arrow_right),
-                    onTap: () {
-                      if (!udc.isLogin()) {
-                        Get.to(const AuthPage());
-                      }
-                    }))),
-        Card(
-          margin: const EdgeInsets.all(2),
-          child: Column(
-            children: [
-              ListTile(
-                leading: const Icon(Icons.settings),
-                title: const Text("设置"),
-                onTap: () => Get.to(const SettingsPage()),
-              ),
-              ListTile(
-                  leading: const Icon(Icons.logout),
-                  title: const Text("退出登录"),
-                  onTap: () {
-                    udc.removeUser();
-                    scc.clearCartItems();
-                  }),
-              ListTile(
-                  leading: const Icon(Icons.info),
-                  title: const Text("关于"),
-                  onTap: () => showAboutDialog(
-                      context: context,
-                      applicationName: "简单商城",
-                      applicationLegalese: "CopyRight © 2023 Zhihao Zhou")),
-            ],
+    Widget accountSettingCard = Card(
+      margin: const EdgeInsets.all(2),
+      child: Column(
+        children: [
+          ListTile(
+            leading: const Icon(Icons.local_shipping),
+            title: const Text("我的地址薄"),
+            onTap: () => Get.to(const AddressBookPage()),
           ),
-        ),
-      ],
+          ListTile(
+              leading: const Icon(Icons.logout),
+              title: const Text("退出登录"),
+              onTap: () {
+                udc.removeUser();
+                scc.clearCartItems();
+              }),
+        ],
+      ),
     );
+
+    Widget appSettingCard = Card(
+      margin: const EdgeInsets.all(2),
+      child: Column(
+        children: [
+          ListTile(
+            leading: const Icon(Icons.settings),
+            title: const Text("设置"),
+            onTap: () => Get.to(const SettingsPage()),
+          ),
+          ListTile(
+              leading: const Icon(Icons.info),
+              title: const Text("关于"),
+              onTap: () => showAboutDialog(
+                  context: context,
+                  applicationName: "简单商城",
+                  applicationLegalese: "CopyRight © 2023 Zhihao Zhou")),
+        ],
+      ),
+    );
+
+    Widget myPageListView = Obx(() => Column(
+          children: [
+            Card(
+                child: Padding(
+                    padding:
+                        const EdgeInsets.symmetric(vertical: 16, horizontal: 8),
+                    child: ListTile(
+                        leading: CircleAvatar(
+                          radius: 30,
+                          backgroundImage:
+                              Image.asset("images/avatar.png").image,
+                        ),
+                        title: udc.isLogin()
+                            ? Text(udc.username.value,
+                                style: const TextStyle(fontSize: 24))
+                            : const Text("未登录，请先登录"),
+                        trailing: const Icon(Icons.keyboard_arrow_right),
+                        onTap: () {
+                          if (!udc.isLogin()) {
+                            Get.to(const AuthPage());
+                          }
+                        }))),
+            Obx(() => AnimatedSwitcher(
+                  duration: const Duration(milliseconds: 150),
+                  transitionBuilder: (child, animation) => SizeTransition(
+                    sizeFactor: animation,
+                    child: child,
+                  ),
+                  child: udc.isLogin()
+                      ? accountSettingCard
+                      : const SizedBox.shrink(),
+                )),
+            appSettingCard,
+          ],
+        ));
 
     return Scaffold(
         appBar: AppBar(title: const Text("我的")),
