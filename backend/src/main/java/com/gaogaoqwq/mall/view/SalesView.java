c11ba3809c80fd3be641fa780b8fd827128fb3de
gaogao-qwq <gaogaoqwq@gmail.com>
2024-05-31 22:39:54 +0800

    Feat: Implement fromMap factory method
    Signed-off-by: gaogao-qwq <gaogaoqwq@gmail.com>

--- backend/src/main/java/com/gaogaoqwq/mall/view/SalesView.java
+++ backend/src/main/java/com/gaogaoqwq/mall/view/SalesView.java
@@ -1,5 +1,12 @@
 package com.gaogaoqwq.mall.view;
 
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -9,6 +16,18 @@ import lombok.Data;
 @AllArgsConstructor
 public class SalesView {
 
+    private String date;
+
     private Long sales;
+
+    public static List<SalesView> fromMap(Map<Date, Long> map) {
+        DateFormat formatter = new SimpleDateFormat("yyyy-MM-dd");
+        return map.entrySet().stream()
+                .map(entry -> SalesView.builder()
+                        .date(formatter.format(entry.getKey()))
+                        .sales(entry.getValue())
+                        .build())
+                .collect(Collectors.toList());
+    }
 
 }
